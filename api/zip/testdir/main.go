7f6bdca33d3273f08c17e073f2c4b1e8238b6e0a
sam.z <45636987@qq.com>
2025-10-30 22:23:33 +0800

    add zip

--- api/zip/testdir/main.go
+++ api/zip/testdir/main.go
@@ -1,3 +1,42 @@
+// package main
+
+// import (
+// 	"archive/zip"
+// 	"fmt"
+// 	"io"
+// 	"log"
+// 	"os"
+// )
+
+// func main() {
+// 	fileName := "test.zip"
+// 	r, err := zip.OpenReader(fileName)
+// 	if err != nil {
+// 		// fmt.Printf("Content %s:\n", f.Name)
+// 		fmt.Printf("打开文件失败 %s", fileName)
+// 		log.Fatal(err)
+// 	}
+// 	defer r.Close()
+
+// 	for _, f := range r.File {
+// 		fmt.Printf("Content %s:\n", f.Name)
+// 		rc, err := f.Open()
+// 		if err != nil {
+// 			fmt.Printf("文件解析失败")
+// 			log.Fatal(err)
+// 		}
+
+// 		_, err = io.CopyN(os.Stdout, rc, 68)
+// 		if err != nil {
+// 			fmt.Printf("文件解析过程中失败")
+// 			log.Fatal(err)
+// 		}
+
+// 		rc.Close()
+// 		fmt.Println()
+// 	}
+// }
+
 package main
 
 import (
