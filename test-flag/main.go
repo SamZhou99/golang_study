c7401d2708b3e2378f2370e96a9e3299f8c079f4
sam.z <45636987@qq.com>
2025-11-01 20:44:26 +0800

    Update main.go

--- test-flag/main.go
+++ test-flag/main.go
@@ -5,6 +5,12 @@ import (
 	"fmt"
 )
 
+// 命令运行示例
+// main.exe -a A的参数 -b B的参数
+// 命令行参数的使用
+// a =  A的参数
+// b =  B的参数
+
 var aValue *string = flag.String("a", "aValue", "测试 a 参数")
 var bValue *string = flag.String("b", "bValue", "测试 b 参数")
 
