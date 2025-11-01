c2fac0473918049aa8b4dffc71544d728b0c9db4
sam.z <45636987@qq.com>
2025-11-01 22:02:14 +0800

    Update main.go

--- test-goroutine/main.go
+++ test-goroutine/main.go
@@ -8,18 +8,19 @@ import (
 var count int32
 
 func say(n int32) {
-	for i := 0; i < 600; i++ {
+	for i := 0; i < 1000; i++ {
 		count += n
 		// fmt.Println(n, i, count)
-		// time.Sleep(10 * time.Millisecond)
+		// time.Sleep(5 * time.Millisecond)
 	}
 }
 
 func main() {
 	go say(1)
 	go say(2)
-	say(3)
-	time.Sleep(9 * time.Millisecond)
+	go say(2)
+	say(5)
+	time.Sleep(100 * time.Millisecond)
 	fmt.Printf("结果：count=%d", count)
 	time.Sleep(1000 * time.Millisecond)
 }
