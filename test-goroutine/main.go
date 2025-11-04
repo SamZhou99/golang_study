a033893eb36d1cf3135d5f6c9676b40b1f44dddc
sam.z <45636987@qq.com>
2025-11-04 14:57:22 +0800

    Update main.go

--- test-goroutine/main.go
+++ test-goroutine/main.go
@@ -5,22 +5,34 @@ import (
 	"time"
 )
 
-var count int32
+var global_count int = 0
 
-func say(n int32) {
-	for i := 0; i < 1000; i++ {
+func say(n int, c chan int) {
+	var max int = 100000000
+	var total int = n * max
+	var count int = 0
+	var i int = 0
+	for i = 0; i < max; i++ {
 		count += n
 		// fmt.Println(n, i, count)
-		// time.Sleep(5 * time.Millisecond)
+		// time.Sleep(2 * time.Millisecond)
 	}
+	global_count += count
+	c <- count / total
 }
 
 func main() {
-	go say(1)
-	go say(2)
-	go say(2)
-	say(5)
+	c := make(chan int)
+
+	var cInt []int
+	iLen := 36
+	for i := 1; i <= iLen; i++ {
+		go say(i, c)
+		cInt = append(cInt, <-c)
+	}
+
+	fmt.Println(cInt)
+	time.Sleep(10 * time.Millisecond)
+	fmt.Println("global_count =", global_count)
 	time.Sleep(100 * time.Millisecond)
-	fmt.Printf("结果：count=%d", count)
-	time.Sleep(1000 * time.Millisecond)
 }
