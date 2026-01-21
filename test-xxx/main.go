6294f40dc7185abfa0ead37c4f9d2bf1b5752f41
sam.z <45636987@qq.com>
2026-01-21 17:50:14 +0800

    Update main.go

--- test-xxx/main.go
+++ test-xxx/main.go
@@ -1,9 +1,15 @@
 package main
 
-import "strconv"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
-	dayCount := 20
+	dayCount := int(input_number())
+	if dayCount >= 50 {
+		dayCount = 50
+	}
 	currentMoneys := 0
 	totalMoneys := 0
 	for i := 1; i <= dayCount; i++ {
@@ -15,4 +21,20 @@ func main() {
 		totalMoneys += currentMoneys
 		println("第"+strconv.Itoa(i)+"天", "给"+strconv.Itoa(currentMoneys)+"元", "共计"+strconv.Itoa(totalMoneys))
 	}
+}
+
+func input_number() uint64 {
+	for true {
+		var str string
+		fmt.Print("\n请输入一个整数(1-50)：")
+		fmt.Scanln(&str)
+
+		num, err := strconv.ParseUint(str, 10, 32)
+		if err != nil {
+			fmt.Println("请输入有效的整数", err)
+			continue
+		}
+		return num
+	}
+	return 0
 }
