ed7c97809acf2446a9076d18059bc6843fcebdf7
sam.z <45636987@qq.com>
2026-04-16 12:23:03 +0800

    Update main.go

--- web_iris/main.go
+++ web_iris/main.go
@@ -1,17 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/kataras/iris/v12"
-
 	"github.com/kataras/iris/v12/middleware/logger"
 	"github.com/kataras/iris/v12/middleware/recover"
-
-	// "fmt"
-	"strconv"
-	"strings"
 )
 
 var markdownContent = []byte(`==# 文字样式==
@@ -51,49 +49,93 @@ func fbnq(ctx iris.Context) {
 	ctx.View("fbnq.html")
 }
 
+func before(ctx iris.Context) {
+	fmt.Println("before...............")
+	ctx.Next()
+}
+
+func mainHandler(ctx iris.Context) {
+	fmt.Println("main.................")
+	ctx.Next()
+}
+
+func after(ctx iris.Context) {
+	name := ctx.Params().Get("name")
+	fmt.Println(name)
+	ctx.WriteString("username: " + name)
+}
+
 // 这里开始运行
 func main() {
 	app := iris.New()
+	// 设置日志级别
 	app.Logger().SetLevel("debug")
+
+	// 静态文件
 	app.HandleDir("/", "./static")
 
+	// 中间件
 	app.Use(recover.New())
+
+	// 记录日志
 	app.Use(logger.New())
 
+	// html模板
 	app.RegisterView(iris.Django("./views", ".html"))
 
+	// 路由传参
+	app.Get("/username/{name}", before, mainHandler, after)
+
+	// 设置参数
+	app.Get("/profile/{id:int min(1)}", func(ctx iris.Context) {
+		i, e := ctx.Params().GetInt("id")
+		if e != nil {
+			ctx.WriteString("error you input")
+		}
+		ctx.WriteString(strconv.Itoa(i))
+	})
+
+	// 首页
 	app.Handle("GET", "/", func(ctx iris.Context) {
 		ctx.HTML("<h1>Welcome</h1>")
 	})
 
+	// 简单的ping-pong
 	app.Get("/ping", func(ctx iris.Context) {
 		ctx.WriteString("pong")
 	})
 
+	// json 返回
 	app.Get("/hello", func(ctx iris.Context) {
 		ctx.JSON(iris.Map{"message": "Hello Iris!"})
 	})
 
+	// markdown 渲染
 	app.Get("/markdown", func(ctx iris.Context) {
 		ctx.Markdown(markdownContent)
 	})
 
+	// 异步的方式，不等待结果返回
 	app.Get("long_async", func(ctx iris.Context) {
 		// ctxCopy := ctx.Clone()
 		go func() {
 			time.Sleep(5 * time.Second)
 			log.Printf("Done! in path: %s , 这里可操作数据库", ctx.Path())
 		}()
+		ctx.WriteString("long_async done")
 	})
+
+	// 同步的方式，等待结果返回
 	app.Get("/long_sync", func(ctx iris.Context) {
 		time.Sleep(5 * time.Second)
 		log.Printf("Done! in path: %s", ctx.Path())
-		ctx.WriteString("done")
+		ctx.WriteString("long_sync done")
 	})
 
 	// 斐波那契 数列
 	app.Get("/fbnq", fbnq)
 
+	// 客户端信息
 	app.Get("/info", clientInfo)
 
 	app.Run(iris.Addr(":8000"), iris.WithoutServerError(iris.ErrServerClosed))
